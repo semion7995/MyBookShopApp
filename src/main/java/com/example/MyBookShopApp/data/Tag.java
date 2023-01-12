3ac6953e858cfc09e457b0495ac8ed364d6ab9ab
unknown <C:\Users\Semen\AppData\Roaming\The Bat!>
2023-01-12 18:18:59 +0300

    add backend Authors books popular and backend tags view thymeleaf main page

---
+++ src/main/java/com/example/MyBookShopApp/data/Tag.java
@@ -0,0 +1,13 @@
+package com.example.MyBookShopApp.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+    private String tag;
+
+}
